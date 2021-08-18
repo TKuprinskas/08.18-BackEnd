5b851ddc07678b50458c9128cf70787e46684703
Tomas Kuprinskas <t.kuprinskas@gmail.com>
2021-08-18 19:01:39 +0300

    commit

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// DB Connection
+
+const URI = `mongodb+srv://tomask:${process.env.MONGODBPW}@cluster0.rjxok.mongodb.net/demo3?retryWrites=true&w=majority`;
+
+const client = new MongoClient(URI);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
