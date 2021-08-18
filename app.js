795348d2fea2bc02fb2f66247abffe842ffef58d
Tomas Kuprinskas <t.kuprinskas@gmail.com>
2021-08-18 18:58:16 +0300

    commit

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const express = require("express");
-const cors = require("cors");
-const { MongoClient } = require("mongodb");
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
 
-const 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
