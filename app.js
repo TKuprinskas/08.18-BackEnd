f7035f5b499c2e6a60e62740aeaffd19fdf1fb2b
Tomas Kuprinskas <t.kuprinskas@gmail.com>
2021-08-18 19:08:24 +0300

    commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient } = require('mongodb');
+const { MongoClient, Db } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -12,6 +12,17 @@ app.use(express.json());
 const URI = `mongodb+srv://tomask:${process.env.MONGODBPW}@cluster0.rjxok.mongodb.net/demo3?retryWrites=true&w=majority`;
 
 const client = new MongoClient(URI);
+
+app.get('/', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con.db('demo3').collection('products').find().toArray();
+    await con.close();
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({ err: 'Please try again' });
+  }
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
